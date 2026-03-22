1565962b9a5f86d978bb49d1f9da2d2bb28eea39
fahmad <fahmad@unfpa.org>
2026-03-22 20:02:44 +0000

    update version

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,21 +1,21 @@
 { pkgs, ... }: {
   channel = "stable-24.05";
   packages = [
-    pkgs.nodejs_20
+    pkgs.nodejs_22
   ];
   idx = {
     extensions = [
-      "angular.ng-template"
-      "vscode.typescript-language-features"
+      "angular.ng-template",
+      "vscode.typescript-language-features",
       "dbaeumer.vscode-eslint"
     ];
     workspace = {
       onCreate = {
-        npm-install = "cd travel-planner && npm install";
+        npm-install = "cd travel-planner && npm install --legacy-peer-deps";
       };
     };
     previews = {
-      enable = true;
+      enable = true,
       previews = {
         web = {
           command = ["sh" "-c" "cd travel-planner && npm run start -- --port $PORT --host 0.0.0.0 --disable-host-check"];
